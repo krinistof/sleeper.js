6e851ea7c794cdef08cee0aff4a971946ca6824d
krinistof <kristof0116@gmail.com>
2021-04-15 10:37:01 +0200

    First working GM version

--- sleeper.js
+++ sleeper.js
@@ -1,66 +1,80 @@
 // ==UserScript==
 // @name           Sleeper.js
 // @namespace      http://example.org
-// @description    Sleeper.js: Teams helper
+// @description    Sleeper.js = Teams helper
 // @version        0.0.1
 // @downloadURL    https://github.com/solymosi/npu/releases/latest/download/npu.user.js
 // @include        https://teams.microsoft.com/_#/pre-*
 // ==/UserScript==
 
 (function(){
-let sleeper = {
-	participants: () => {
-		// There may be a better solution.
-		let x = sleeper.spans[1].innerText
-		// The span contains the number between brackets, so we have to remove them
-		return parseInt(x.substr(1, x.length-2))
-	},
-	bye: () => {
-		sleeper.stopChecker()
-		//TODO: Add goodbye message generator, and sending
-		//
-		// Find the hangup button, and click it
-		document.getElementById("hangup-button").click()
 
-	},
-	check: () => {
-		if(sleeper.participants() < sleeper.threshold)
-			sleeper.bye()
-	},
-	start: () => {
-		sleeper.init()
-		sleeper.startChecker()
-	},
-	init: () => {
-		//Getting the array of toggle-number class spans
-		sleeper.spans = document.querySelectorAll("span.toggle-number")
+let spans, threshold, intId, element
 
-		//TODO make method which automatically determines threshold
-		sleeper.threshold = 7
+function participants(){
+	// There may be a better solution.
+	let x = element.innerText
+	// The span contains the number between brackets, so we have to remove them
+	return parseInt(x.substr(1, x.length-2))
+}
 
-		//Checking if participants tab is opened
-		if(!sleeper.spans){
-			//If not, then open it
-			document.getElementById("roster-button").click()
-			setTimeout(sleeper.init, 4000)
+function bye(){
+	stopChecker()
+	//TODO = Add goodbye message generator, and sending
+	//
+	// Find the hangup button, and click it
+	document.getElementById("hangup-button").click()
+}
+
+function check(){
+	if(participants() < threshold)
+		bye()
+}
+
+function start(){
+	init()
+	startChecker()
+}
+
+function init(){
+	//Getting the array of toggle-number class spans
+	spans = document.querySelectorAll("span.toggle-number")
+
+	//TODO make method which automatically determines threshold
+	threshold = 7
+
+	//Checking if participants tab is opened
+	if(spans.length == 0){
+		//If not, then open it
+		let b = document.getElementById("roster-button")
+		if(!b){
+			document.getElementById("callingButtons-showMoreBtn").click()
+			setTimeout(init, 4000)
+			return
 		}
-		sleeper.element = sleeper.spans[1]
-	},
-	startChecker: () => {
-		alert("Checker started!")
-		sleeper.intId = setInterval(sleeper.check, 4000)
-	},
-	stopChecker: () => {
-		console.log("Checker stopped!")
-		clearInterval(sleeper.intId)
+		b.click()
+		setTimeout(init, 4000)
+		return
 	}
+	element = spans[1]
+}
+
+function startChecker(){
+	intId = setInterval(check, 4000)
+	console.log("Checker started with PID: ", intId)
+}
+
+function stopChecker(){
+	console.log("Checker stopped!")
+	clearInterval(intId)
 }
 
 let c = setInterval(() => {
 	//Wait till call is opened
 	if(document.getElementById("hangup-button")){
+		console.log("hívás folyamatban")
 		clearInterval(c)
-		setTimeout(sleeper.start, 7000)
+		setTimeout(start, 7000)
 	}
 }, 2000)
 })()
